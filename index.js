8f5f27bfe6cf622998e604af101fcea9444d1d0b
tylersmith <tyjsmith1@gmail.com>
2023-10-31 18:02:04 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution in this file!
+
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (multiplier) {
+    return function fareMultiplier (fare) {
+        return fare * multiplier
+    }
+}
+
+function fareDoubler (fare) {
+    return createFareMultiplier(2)(fare)
+}
+
+function fareTripler (fare) {
+    return createFareMultiplier(3)(fare)
+}
+
+let selectDifferentDrivers = (drivers,fn) => {
+    return fn(drivers)
+}
+
+
+
+// code
+// let returnFirstTwoDrivers = (names) => {
+//     return names.slice(0,2);
+// }
+
+// let returnLastTwoDrivers = (names) => {
+//     return names.slice(2,4);
+// }
+
+// let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// function createFareMultiplier (multiplier) {
+//     return function fareMultiplier (fare) {
+//         return fare * multiplier;
+//     }
+// }
+
+// let fairDoubler = () => {return createFareMultiplier(2)};
